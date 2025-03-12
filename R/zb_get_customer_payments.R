b694f1aa55d7723c45f4338ac8444aa263e81839
selesnow <selesnow@gmail.com>
2025-03-12 10:57:58 +0200

    Добавил документацию к zb_get_customer_payments

--- R/zb_get_customer_payments.R
+++ R/zb_get_customer_payments.R
@@ -1,3 +1,20 @@
+#' Get Payments Received
+#'
+#' @details
+#' For more infomation see [api docs](https://www.zoho.com/books/api/v3/customer-payments/#list-customer-payments)
+#'
+#'
+#' @param organization_id Organizations ids
+#' @param date_start Search payments by estimate date.
+#' @param date_end Search payments by estimate date.
+#' @param date_before Search payments by estimate date.
+#' @param date_after Search payments by estimate date.
+#' @param search_text Search payments by reference number or customer name or payment description.
+#' @param filter_by Filter payments by mode.Allowed Values: PaymentMode.All, PaymentMode.Check, PaymentMode.Cash, PaymentMode.BankTransfer, PaymentMode.Paypal, PaymentMode.CreditCard, PaymentMode.GoogleCheckout, PaymentMode.Credit, PaymentMode.Authorizenet, PaymentMode.BankRemittance, PaymentMode.Payflowpro, PaymentMode.Stripe, PaymentMode.TwoCheckout, PaymentMode.Braintree and PaymentMode.Others
+#'
+#' @returns tibble
+#' @export
+#'
 zb_get_customer_payments <- function(
   organization_id,
   date_start  = NULL,
