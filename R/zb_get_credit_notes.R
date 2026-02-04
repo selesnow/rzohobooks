cef6f62ad8390c103fbd2ef318053d038904585d
selesnow <selesnow@gmail.com>
2026-02-04 16:49:48 +0200

    В функцию zb_get_credit_notes() добавлена возможность фильтрации по датам и аргумент для поиска.

--- R/zb_get_credit_notes.R
+++ R/zb_get_credit_notes.R
@@ -1,6 +1,11 @@
 #' Get credit notes
 #'
 #' @param organization_id Organizations ids
+#' @param date_start Search credit notes by bill date.
+#' @param date_end Search credit notes by bill date.
+#' @param date_before Search credit notes by bill date.
+#' @param date_after Search credit notes by bill date.
+#' @param search_text Search credit notes using text search across multiple fields. Searches credit note number, customer name, and reference number. Max-length 100
 #'
 #' @details
 #' See [api docs](https://www.zoho.com/books/api/v3/credit-notes/#list-all-credit-notes)
@@ -14,14 +19,24 @@
 #' contacts <- zb_get_credit_notes(organizations$organization_id)
 #' }
 zb_get_credit_notes <- function(
-    organization_id
+    organization_id,
+    date_start  = NULL,
+    date_end    = NULL,
+    date_before = NULL,
+    date_after  = NULL,
+    search_text = NULL
 ) {
   suppressMessages({
     result <- map_dfr(organization_id,
                       \(x) {
                         zb_make_request(
                           endpoint = 'creditnotes',
-                          organization_id = x
+                          organization_id = x,
+                          date_start      = date_start,
+                          date_end        = date_end,
+                          date_before     = date_before,
+                          date.after      = date_after,
+                          search_text     = search_text
                         ) %>%
                           zb_parse() %>%
                           mutate(organization_id = x)
